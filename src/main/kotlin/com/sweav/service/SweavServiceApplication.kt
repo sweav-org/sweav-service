3456b6b411872726191a8f1708156184bcd3a7dc
Kina <kina827@naver.com>
2025-02-21 09:31:02 +0900

    Initial commit

---
+++ src/main/kotlin/com/sweav/service/SweavServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sweav.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SweavServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SweavServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/sweav/service/SweavServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sweav.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SweavServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
